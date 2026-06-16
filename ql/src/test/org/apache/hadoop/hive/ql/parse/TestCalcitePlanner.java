0c4c0138011fd6740d47f2b9f8a40e325f4201c6
Thomas Rebele <thomas.rebele@gmail.com>
2026-06-16 16:54:46 +0200

    refactor

--- ql/src/test/org/apache/hadoop/hive/ql/parse/TestCalcitePlanner.java
+++ ql/src/test/org/apache/hadoop/hive/ql/parse/TestCalcitePlanner.java
@@ -20,47 +20,34 @@ package org.apache.hadoop.hive.ql.parse;
 
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConfForTest;
-import org.apache.hadoop.hive.metastore.api.Database;
-import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.ql.Context;
 import org.apache.hadoop.hive.ql.QueryState;
 import org.apache.hadoop.hive.ql.exec.Operator;
-import org.apache.hadoop.hive.ql.metadata.Hive;
-import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.metadata.Table;
+import org.apache.hadoop.hive.ql.security.authorization.plugin.sqlstd.SQLStdConfOnlyAuthorizerFactory;
 import org.apache.hadoop.hive.ql.session.SessionState;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertTrue;
 
 public class TestCalcitePlanner {
-  public static final String EXPLAIN_CONF_VAR = "hive.log.explain.output";
   static QueryState queryState;
   static HiveConf conf;
 
   ParseDriver pd;
   SemanticAnalyzer sA;
-  private static Hive db;
 
   @BeforeClass
   public static void initialize() throws Exception {
     conf = new HiveConfForTest(TestCalcitePlanner.class);
     conf.set("hive.security.authorization.enabled", "false");
-    conf.set("hive.security.authorization.manager",
-        "org.apache.hadoop.hive.ql.security.authorization.plugin.sqlstd.SQLStdConfOnlyAuthorizerFactory");
-    conf.set(EXPLAIN_CONF_VAR, "true");
-    queryState =
-        new QueryState.Builder().withHiveConf(conf).build();
+    conf.set("hive.security.authorization.manager", SQLStdConfOnlyAuthorizerFactory.class.getCanonicalName());
+    conf.set(HiveConf.ConfVars.HIVE_LOG_EXPLAIN_OUTPUT.varname, "true");
+    queryState = new QueryState.Builder().withHiveConf(conf).build();
 
     SessionState.start(conf);
   }
-
 
   @Before
   public void setup() throws SemanticException {
@@ -75,9 +62,7 @@ public class TestCalcitePlanner {
 
   @Test
   public void testExtractSubQueries() throws Exception {
-    HiveConf.ConfVars var = HiveConf.ConfVars.HIVE_LOG_EXPLAIN_OUTPUT;
-    boolean boolVar = conf.getBoolVar(var);
-    assertTrue(boolVar);
+    assertTrue(conf.getBoolVar(HiveConf.ConfVars.HIVE_LOG_EXPLAIN_OUTPUT));
 
     ASTNode ast = parse("select 1 from table(values(1)) as t(a)");
     CalcitePlanner calcitePlanner = new CalcitePlanner(queryState);
